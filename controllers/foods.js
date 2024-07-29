b34ac0c454314068245998da027fe2122c545964
Jessica Ramos <jessica.n.ramos@me.com>
2024-07-29 07:17:20 -0400

    fixed delete functionality

--- controllers/foods.js
+++ controllers/foods.js
@@ -37,8 +37,11 @@ router.post('/', async (req, res) => {
 router.delete('/:itemId', async (req, res) => {
   try {
     const currentUser = await User.findById(req.session.user._id);
-    currentUser.pantry.id(req.params.itemId).remove();
+    
+
+    currentUser.pantry.pull({ _id: req.params.itemId });
     await currentUser.save();
+
     res.redirect(`/users/${req.session.user._id}/foods`);
   } catch (error) {
     console.log(error);
